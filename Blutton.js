48618b153bff7665a344f7e7f6db5c08335e99f0
paiameya <ameya@rayabharitech.com>
2020-07-01 16:03:51 +0530

    Initial commit

---
+++ Blutton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+export default function Blutton({ styles, children, busy, ...props }) {
+  return (
+<Button type="primary">Primary Button</Button>
+  )
+}
+
